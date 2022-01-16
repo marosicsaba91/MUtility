fa468da79639737d864d795e92edc3318c0daa11
Csaba Marosi <marosicsaba91@gmail.com>
2022-01-16 15:32:53 +0100

    Inspector Update

--- Runtime/InspectorElements/SpecificImplementations/InspectorProperties/BoolProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿using System; 
-
-namespace MUtility
-{
-[Serializable] public abstract class BoolProperty<TParentObject> : InspectorProperty<TParentObject, bool>
-{ }
-
-[Serializable] public class BoolProperty : BoolProperty<object> { }
-}
---
+++ Runtime/InspectorElements/SpecificImplementations/InspectorProperties/InspectorBool.cs
@@ -0,0 +1,9 @@
+﻿using System; 
+
+namespace MUtility
+{
+[Serializable] public abstract class InspectoreBool<TParentObject> : InspectorProperty<TParentObject, bool>
+{ }
+
+[Serializable] public class InspectoreBool : InspectoreBool<object> { }
+}
