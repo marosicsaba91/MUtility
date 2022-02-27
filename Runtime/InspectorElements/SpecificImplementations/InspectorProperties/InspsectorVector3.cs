26cb73a060bd9ccb93c24fa6797b07771de573b8
Csaba Marosi <marosicsaba91@gmail.com>
2022-02-27 14:46:42 +0100

    small fix

--- Runtime/InspectorElements/SpecificImplementations/InspectorProperties/InspsectorVector3.cs
+++ Runtime/InspectorElements/SpecificImplementations/InspectorProperties/InspsectorVector3.cs
@@ -6,5 +6,4 @@ namespace MUtility
 [Serializable] public abstract class InspectorVector3<TParentObject> : InspectorProperty<TParentObject, UnityEngine.Vector3>
 {
 }
-[Serializable] public class Vector3 : InspectorVector2<object> { }
 }
