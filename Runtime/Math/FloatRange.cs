954d62ee363d76e743899f47ac2318863f96b241
Csaba Marosi <marosicsaba91@gmail.com>
2022-04-07 19:36:01 +0200

    FloatRange Lerp

--- Runtime/Math/FloatRange.cs
+++ Runtime/Math/FloatRange.cs
@@ -9,7 +9,10 @@ public class FloatRange
     public float min;
     public float max;
     public float Range => max - min;
-    public float GetRandom() => Random.Range(min, max);
+	
+    public float GetRandom() => Random.Range(min, max);	
+	public float Lerp(float t) => Mathf.Lerp(min, max, t);
+    public float LerpUnclamped(float t) => Mathf.LerpUnclamped(min, max, t);
     
     public override string ToString() => "(" + this.min.ToString() + "/" + this.max.ToString() + ")";
 }
