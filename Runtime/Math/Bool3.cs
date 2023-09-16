d38fa6ae652a546eb38785e3be9e28744d333486
Csaba Marosi <marosicsaba91@gmail.com>
2023-09-16 08:19:46 +0200

    Bool3

---
+++ Runtime/Math/Bool3.cs
@@ -0,0 +1,26 @@
+﻿namespace MUtility
+{
+	public struct Bool3
+	{
+		public bool x, y, z;
+
+		public static readonly Bool3 falseValue = false;
+		public static readonly Bool3 trueValue = true;
+
+		public Bool3(bool x, bool y, bool z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		public Bool3(bool b)
+		{
+			x = b;
+			y = b;
+			z = b;
+		}
+
+		public static implicit operator Bool3(bool b) => new(b, b, b);
+	}
+}
