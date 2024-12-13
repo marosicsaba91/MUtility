72213bcd97617c691b992215dcc0d3d0fc362613
Csaba Marosi <marosicsaba91@gmail.com>
2024-12-13 20:12:21 +0100

    TryGetCopy of any Object

--- Runtime/ExtensionMethods/ObjectExtension.cs
+++ Runtime/ExtensionMethods/ObjectExtension.cs
@@ -1,20 +1,36 @@
 ﻿using System;
+using System.Reflection;
 
 namespace MUtility
 {
 	public static class ObjectExtension
 	{
-		public static T TryGetCopy<T>(this T obj) where T : class
-		{ 
+		public static T TryCopyWithReflection<T>(this T obj, bool copyRecursively) where T : class
+		{
+			if (obj == null) return null;
+
 			Type type = obj.GetType();
 			T copyObj = Activator.CreateInstance(type) as T;
 
-			foreach (System.Reflection.PropertyInfo item in type.GetProperties())
-				if (item.GetSetMethod() != null && item.GetGetMethod() != null)
-					item.SetValue(copyObj, item.GetValue(obj));
+			foreach (PropertyInfo propertyInfo in type.GetProperties())
+				if (propertyInfo.GetSetMethod() != null && propertyInfo.GetGetMethod() != null)
+				{
+					object item = propertyInfo.GetValue(obj);
 
-			foreach (System.Reflection.FieldInfo item in type.GetFields())
-				item.SetValue(copyObj, item.GetValue(obj));
+					if (copyRecursively && propertyInfo.PropertyType.IsClass)
+						item = item.TryCopyWithReflection(copyRecursively);
+
+					propertyInfo.SetValue(copyObj, item);
+				}
+			foreach (FieldInfo fieldInfo in type.GetFields())
+			{
+				object item = fieldInfo.GetValue(obj);
+
+				if (copyRecursively && fieldInfo.FieldType.IsClass)
+					item = item.TryCopyWithReflection(copyRecursively);
+
+				fieldInfo.SetValue(copyObj, item);
+			}
 
 			return copyObj;
 		}
