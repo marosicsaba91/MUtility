140646876606771f78074e140090edd0b9271696
Csaba Marosi <marosicsaba91@gmail.com>
2025-08-28 20:53:19 +0200

    Samll Update

--- Runtime/ExtensionMethods/RandomExtensions.cs
+++ Runtime/ExtensionMethods/RandomExtensions.cs
@@ -4,12 +4,17 @@ namespace MUtility
 {
 	public static class RandomExtensions
 	{
-		public static Vector2 InsideUnitCircle(this System.Random random) 
+		public static Vector2 InsideUnitCircle(this System.Random random)
 		{
 			float angle = (float)(random.NextDouble() * Mathf.PI * 2);
 			float radius = (float)(random.NextDouble());
 			radius = Mathf.Sqrt(radius); // To ensure uniform distribution within the circle
 			return new Vector2(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius);
+		}
+		public static Vector2 OnUnitCircle(this System.Random random)
+		{
+			float angle = (float)(random.NextDouble() * Mathf.PI * 2);
+			return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
 		}
 	}
 }
